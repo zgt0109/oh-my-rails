8d8127ca33c1bed845fe741b5409ee36b89087aa
guxiaobai <sikuan.gu@gmail.com>
2016-10-27 18:08:58 +0800

    get public key

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,17 +4,17 @@
 GEM_SOURCES_CHINA=https://gems.ruby-china.org/
 GEM_SOURCES_ORIGIN=https://rubygems.org/
 
-rsa_pub_guxiaobai="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiAP0QTzqGB4Mqhq+nyx\
-00grcmLNqDV8ttZnjd/D3yC1Km9speqrWejSOQlpOHfCIARUIiAKpfwjYlFHT1nZ9+5czRKqNljaT\
-5OKlF+RYyrrlxi9NcmjCzf0h5soAXcCpe1xUSDFaZjSvcTTJFXZyyo9GWRaexAan9jlpH4iZd7ty8\
-rsQX/vHFwujSnyrLbGyOiG7eldVZt7+HoCdJ12SsC2boTFlwy1hYlk0t25Kx3dU9BCupuVQ/nbavf\
-zZpbSx0vqCMqekqj1/r/zlVLRDrCzRau3bKInmsHT3JlMRjmLXC+HvxB2+1LKMINcbCKaWJ/DYD0W\
-81EjIuPM6thTUT sikuan.gu@gmail.com"
+# rsa_pub_guxiaobai="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiAP0QTzqGB4Mqhq+nyx\
+# 00grcmLNqDV8ttZnjd/D3yC1Km9speqrWejSOQlpOHfCIARUIiAKpfwjYlFHT1nZ9+5czRKqNljaT\
+# 5OKlF+RYyrrlxi9NcmjCzf0h5soAXcCpe1xUSDFaZjSvcTTJFXZyyo9GWRaexAan9jlpH4iZd7ty8\
+# rsQX/vHFwujSnyrLbGyOiG7eldVZt7+HoCdJ12SsC2boTFlwy1hYlk0t25Kx3dU9BCupuVQ/nbavf\
+# zZpbSx0vqCMqekqj1/r/zlVLRDrCzRau3bKInmsHT3JlMRjmLXC+HvxB2+1LKMINcbCKaWJ/DYD0W\
+# 81EjIuPM6thTUT sikuan.gu@gmail.com"
 
 secret_key="c6cf1a911977bd40ffef2a3cee6d062703e0322ef4885534694a50f9c704e2aefa\
 8af3c4ed4d4a1fe5900101895da6b54973c7f8490821ded451b28a5305e526"
 
-
+read -p "Public Key? " rsa_pub_guxiaobai
 read -p "Deploy User Name? " user_name
 read -p "Application Name? " app_name
 
